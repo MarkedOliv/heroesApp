d0e989853a2f6fe7bb7171fd34f6729d460e0d31
MarkedOliv <olivdev4@gmail.com>
2023-07-19 16:09:32 -0400

    se agrega la funcionalidad de el buscador y las vistas de el formulario de creación del héroe

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { HeroesRoutingModule } from './heroes-routing.module';
 
@@ -30,6 +31,7 @@ import { HeroImagePipe } from './pipes/hero-image.pipe';
     CommonModule,
     HeroesRoutingModule,
     MaterialModule,
+    ReactiveFormsModule,
   ]
 })
 export class HeroesModule { }
